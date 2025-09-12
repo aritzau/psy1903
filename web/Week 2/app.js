2abe1a14115f499fb7843bf9813d5380e4be7ff5
Annabella Ritzau <aritzau@college.harvard.edu>
2025-09-12 19:30:52 -0400

    experiment

--- web/Week 2/app.js
+++ web/Week 2/app.js
@@ -1,4 +1,66 @@
-console.log('Hello World');
+let welcomeMessage = 'Welcome to our experiment. Please read each question the instruction carefully. '
+let experiment = 'stroop';
+console.log(typeof experiment);
+console.log(experiment.charAt(1));
+
+
+
+
+let trialCount = 20;
+console.log(typeof trialCount);
+
+
+
+
+let colors = ['red', 'green', 'blue',];
+
+alert('welcome to the ' + experiment + 'experiment');
+
+trialCount = 40;
+
+let halfwayCount = trialCount / 2;
+
+console.log(halfwayCount);
+
+let correct = true;
+console.log(typeof correct);
+
+const course = "courseName";
+console.log(course.toLowerCase());
+console.log(course.toUpperCase());
+
+let courseName = "Introduction to Psychologists and Their Research";
+
+let index = courseName.indexOf("Psychologists");
+
+console.log(index);
+
+const course_Name = "Programming for Psychologist";
+const updatedCourseName = course_Name.replace("Programming", "Coding");
+
+console.log(updatedCourseName);
+console.log(course_Name);
+
+console.log(updatedCourseName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ web/Week 2/mrt.js
@@ -0,0 +1,17 @@
+
+<script src="mrt.js" defer></script>
+
+alert("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.");
+
+let answer1 = prompt("What is 1 + 3?");
+alert("You answered " + answer1);
+
+
+let answer2 = prompt("What is 5 + 7?");
+alert("You answered " + answer2);
+
+let answer3 = prompt("What is 8 + 6?");
+alert("You answered " + answer3);
+
+alert("Thank you for your participation!");
+
