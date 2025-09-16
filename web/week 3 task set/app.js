cc5541279aab615ea3adb9336407d124037a2024
Annabella Ritzau <aritzau@college.harvard.edu>
2025-09-16 17:31:22 -0400

    commit

--- web/week 3 task set/app.js
+++ web/week 3 task set/app.js
@@ -10,3 +10,38 @@ num10uput.innerHTML = randomNum1
 num20uput.innerHTML = randomNum2
 //update elements on the page
 
+// rewriting code so that the two numbers are randomly generated
+let num1 = Math.floor(Math.random() * 100) + 1;
+let num2 = Math.floor(Math.random() * 100) + 1;
+
+let response = prompt(`What is  ${num1}  +  ${num2}  ?`);
+let CorrectAnswer = num1 + num2
+
+if (response == CorrectAnswer) {
+    alert("Correct!");
+
+} else if (response != CorrectAnswer) {
+    alert("incorrect!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
