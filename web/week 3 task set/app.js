7fa20fd187753a791b6b699080838b383cb1b87a
Annabella Ritzau <aritzau@college.harvard.edu>
2025-09-18 19:41:42 -0400

    math 11 experiment

--- web/week 3 task set/app.js
+++ web/week 3 task set/app.js
@@ -10,78 +10,45 @@ num10uput.innerHTML = randomNum1
 num20uput.innerHTML = randomNum2
 //update elements on the page
 
-//Random number
-let start = Date.now();
-let num1 = Math.floor(Math.random() * 100) + 1;
-let num2 = Math.floor(Math.random() * 100) + 1;
-let response = prompt(`What is  ${num1}  +  ${num2}  ?`);
-let end = Date.now();
-let TimetoRespond = (end - start) / 1000;
-let CorrectAnswer = num1 + num2
 
-if (response == CorrectAnswer) {
-    alert("You answered " + response + " in " + TimetoRespond + " seconds. Your Response was CORRECT");
-} else if (response != CorrectAnswer) {
-    alert("You answered " + response + " in " + TimetoRespond + " seconds. Your Response was INCORRECT");
-}
+// Create variables to store references to elements on the page
+let num1s = document.getElementsById('num1');
+let num2s = document.getElementById('num2');
+let response1 = document.getElementById("response");
+let feedback = document.getElementById("Feedback");
+let form = document.getElementById("form")
 
-let start2 = Date.now();
-let num3 = Math.floor(Math.random() * 100) + 1;
-let num4 = Math.floor(Math.random() * 100) + 1;
-let Response2 = prompt(`What is  ${num3}  +  ${num4}  ?`);
-let end2 = Date.now();
-let TimetoRespond2 = (end - start) / 1000;
-let CorrectAnswer2 = num3 + num4
+//generating numbers
+let Num1 = Math.floor(Math.random() * 100) + 1;
+let Num2 = Math.floor(Math.random() * 100) + 1;
+let correctAnswer = Num1 + Num2;
 
-if (Response2 == CorrectAnswer2) {
-    alert("You answered " + Response2 + " in " + TimetoRespond2 + " seconds. Your Response was CORRECT");
-} else if (Response2 != CorrectAnswer2) {
-    alert("You answered " + Response2 + " in " + TimetoRespond2 + " seconds. Your Response was INCORRECT");
-}
+//putting numbers on a page
+num1s.textContent = Num1;
+num2s.textContent = Num2;
 
-let start3 = Date.now();
-let num5 = Math.floor(Math.random() * 100) + 1;
-let num6 = Math.floor(Math.random() * 100) + 1;
-let Response3 = prompt(`What is  ${num5}  +  ${num6}  ?`);
-let end3 = Date.now();
-let TimetoRespond3 = (end - start) / 1000;
-let CorrectAnswer3 = num5 + num6
+let Start = Date.now();
 
-if (Response3 == CorrectAnswer3) {
-    alert("You answered " + Response3 + " in " + TimetoRespond3 + " seconds. Your Response was CORRECT");
-} else if (Response3 != CorrectAnswer3) {
-    alert("You answered " + Response3 + " in " + TimetoRespond3 + " seconds. Your Response was INCORRECT");
-}
+//form submission
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    form.style.display = 'none';
+    let end = Date.now();
+
+    let TimetoRespond = (end - Start) / 1000;
+
+    let response = Number(response.value);
 
 
+    if (response == correctAnswer) {
 
+        feedback.textContent = "You answered " + response + " in " + TimetoRespond + " seconds. Your Response was CORRECT";
 
-let age = prompt('How old are you?');
-if (age < 12) {
-    alert('Child');
-}
-if (age >= 12 && age < 30) {
-    alert('Teenager');
-}
-if (age >= 30) {
-    alert("Adult");
-}
+    } else if (response != correctAnswer) {
+        feedback.textContent = "You answered " + response + " in " + TimetoRespond + " seconds. Your Response was INCORRECT";
 
-let WholeNumber = prompt("Please enter a whole number");
-if (WholeNumber % 2 == 0) {
-    alert("The number you entered was even")
-} else {
-    alert("The number you entered was odd");
-}
+    }
 
-
-//random number with feedback
-///alert("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.");
-
-
-
-
-
-
+})
 
 
