061046b24de1b2b7ef93ce4029d7f510b746d55d
Annabella Ritzau <aritzau@college.harvard.edu>
2025-09-25 20:20:18 -0400

    converTemp

--- web/Week4-task-set/app.js
+++ web/Week4-task-set/app.js
@@ -1,4 +1,23 @@
+/*
+celsiusToFharenheit
 function celsiusToFahrenheit(celsius) {
     return (celsius * 1.8) + 32;
 }
 
+console.log(celsiusToFahrenheit(10))
+*/
+
+
+//convertTemp
+function convertTemp(temp, convertTo) {
+    if (convertTo == "c") {
+        // Fahrenheit to Celsius conversion
+        return (temp - 32) / 1.8;
+    }
+    else {
+        (convertTo == "f")
+        // Celsius to Fahrenheit conversion
+        return (temp * 1.8) + 32;
+    }
+}
+console.log(convertTemp(10, 'c'));
