0dd604849c65e22f02c99884b921905ae053f18f
Annabella Ritzau <aritzau@college.harvard.edu>
2025-10-16 23:43:20 -0400

    project 1

---
+++ web/Project-1/conditions.js
@@ -0,0 +1,26 @@
+// Define the word pool - ideally 20+ words for flexibility
+let wordPool = [
+    "apple", "table", "ocean", "cloud", "chair", "house", "banana", "garden",
+    "lemon", "door", "window", "flower", "train", "biscuit", "zipper",
+    "tree", "pencil", "pillow", "street", "camera"
+];
+
+// Always the same target
+let targetWord = "ocean";
+
+// 5 trials at each cloud size (1, 5, 10)
+let experimentConditions = [
+    { cloudSize: 1 }, { cloudSize: 1 }, { cloudSize: 1 }, { cloudSize: 1 }, { cloudSize: 1 },
+    { cloudSize: 5 }, { cloudSize: 5 }, { cloudSize: 5 }, { cloudSize: 5 }, { cloudSize: 5 },
+    { cloudSize: 10 }, { cloudSize: 10 }, { cloudSize: 10 }, { cloudSize: 10 }, { cloudSize: 10 }
+];
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
